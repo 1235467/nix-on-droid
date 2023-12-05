746fdb4128933fdd542b66ddb7125a81053539bc
Your Name <you@example.com>
2023-12-05 02:57:54 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Basic example of Nix-on-Droid system config.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-on-droid }: {
+
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      modules = [ ./nix-on-droid.nix ];
+    };
+
+  };
+}
---
+++ nix-on-droid.nix
@@ -0,0 +1,59 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.packages = with pkgs; [
+    vim # or some other editor, e.g. nano or neovim
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+    fish
+    nixpkgs-fmt
+    libgourou
+    openssh
+    rsync
+    git
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "23.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+    
+  '';
+
+  # Set your time zone
+  time.timeZone = "China/Shanghai";
+  user.shell = "${pkgs.fish}/bin/fish";
+
+  home-manager = {
+    useGlobalPkgs = true;
+  #
+  #  config =
+  #    { config, lib, pkgs, ... }:
+  #    {
+  #      # Read the changelog before changing this value
+  #      home.stateVersion = "23.05";
+  #
+  #      # insert home-manager config
+  #    };
+  };
+}
+
+# vim: ft=nix
