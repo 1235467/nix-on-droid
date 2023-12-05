79c8f8109072731c8262206324b67102e699a350
hakutaku <qq20099@outlook.com>
2023-12-05 22:14:11 +0800

    add some packages

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -29,6 +29,8 @@
     openssh
     rsync
     inputs.nur-custom.legacyPackages.aarch64-linux.jjwxcCrawler
+    inputs.nur-custom.legacyPackages.aarch64-linux.swgp-go
+    inputs.nur-custom.legacyPackages.aarch64-linux.ab-av1
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
