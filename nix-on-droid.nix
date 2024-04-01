e94b0311e705125f86972314dcb610d889963689
hakutaku <qq20099@outlook.com>
2024-04-02 00:24:42 +0800

    edit

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -1,17 +1,9 @@
 { config, lib, pkgs, inputs, ... }:
-
+let
+  nurpkg = inputs.nur-custom.legacyPackages.aarch64-linux;
+in
 {
   environment.packages = with pkgs; [
-    # or some other editor, e.g. nano or neovim
-    #     diffutils
-    #     findutils
-    #     utillinux
-    #     tzdata
-    #     hostname
-    #     gnugrep
-    #     gnupg
-    #     gnused
-    #     gnutar
     libgourou
     git
     nixpkgs-fmt
@@ -32,12 +24,12 @@
     ffmpeg
     doas
     wget 	
-    inputs.nur-custom.legacyPackages.aarch64-linux.jjwxcCrawler
-    inputs.nur-custom.legacyPackages.aarch64-linux.ncmdump-rs
-    inputs.nur-custom.legacyPackages.aarch64-linux.reflac
-    inputs.nur-custom.legacyPackages.aarch64-linux.DownOnSpot
-    inputs.nur-custom.legacyPackages.aarch64-linux.BBDown
-    
+    nurpkg.jjwxcCrawler
+    nurpkg.ncmdump-rs
+    nurpkg.reflac
+    nurpkg.DownOnSpot
+    nurpkg.BBDown
+    nurpkg.koboldcpp
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
@@ -52,10 +44,6 @@
   '';
   nix.substituters = ["https://cache.nixos.org" "https://cache.garnix.io" ];
   nix.trustedPublicKeys = [
-    #"nixbuild.net/nixbuild-1:RTmcV6NzBWCJgfq5rxZigu12jz0HZf18uh0wh/dL4O0="
-    #"numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-    #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    #"hakutaku-cache.cachix.org-1:xO0APxxyhJqmN29tIEBuqjhbrx3UIYxMfZ70SY6RuVo="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
   ];
